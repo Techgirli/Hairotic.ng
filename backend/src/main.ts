d66b71bc16823823aa5fa6e9a1b7df285aba19a0
techgirli <olorunsolamiracle@gmail.com>
2026-07-16 20:28:33 -0400

    modified some backend files

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
+import { RequestMethod } from '@nestjs/common';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 
@@ -7,7 +8,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { rawBody: true });
 
   // Set global API prefix
-  app.setGlobalPrefix('api/v1');
+  app.setGlobalPrefix('api/v1', {
+    exclude: [{ path: '/', method: RequestMethod.GET }],
+  });
 
   // Register cookie parser
   app.use(cookieParser());
